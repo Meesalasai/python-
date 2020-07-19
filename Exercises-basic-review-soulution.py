dab2a901745c4715876043abfd0de714645fa547
Meesalasai <58086269+Meesalasai@users.noreply.github.com>
2020-07-19 16:44:51 +0530

    Update Exercises-basic-review-soulution.py

--- Exercises-basic-review-soulution.py
+++ Exercises-basic-review-soulution.py
@@ -1,15 +1,5 @@
-#####################################
-#### PART 6: EXERCISE REVIEW  #######
-#####################################
 
-# Time to review all the basic data types we learned! This should be a
-# relatively straight-forward and quick assignment.
 
-###############
-## Problem 1 ##
-###############
-
-# Given the string:
 s = 'django'
 
 # Use indexing to print out the following:
@@ -26,19 +16,12 @@ print(s[4:])
 # Bonus: Use indexing to reverse the string
 print(s[::-1])
 
-###############
-## Problem 2 ##
-###############
 
 # Given this nested list:
 l = [3,7,[1,4,'hello']]
 # Reassign "hello" to be "goodbye"
 rl  = l[2][2] = 'goodbye'
 print(l)
-
-###############
-## Problem 3 ##
-###############
 
 # Using keys and indexing, grab the 'hello' from the following dictionaries:
 
@@ -49,17 +32,12 @@ print(d2['k1']['k2'])
 d3 = {'k1':[{'nest_key':['this is deep',['hello']]}]}
 print(d3['k1'][0]['nest_key'][1][0])
 
-###############
-## Problem 4 ##
-###############
 
 # Use a set to find the unique values of the list below:
 mylist = [1,1,1,1,1,2,2,2,2,3,3,3,3]
 myset = set(mylist)
 print(myset)
-###############
-## Problem 5 ##
-###############
+
 
 # You are given two variables:
 age = 4
